0586b2ea45b62cacdcc8dfee847a38659775c730
segundo <segujuan27@hotmail.com>
2024-03-15 14:05:09 +0100

    mdx-bund

--- src/lib/mdx-server.ts
+++ src/lib/mdx-server.ts
@@ -2,26 +2,21 @@ import { bundleMDX } from "mdx-bundler"
 import rehypeAutolinkHeadings from "rehype-autolink-headings"
 import rehypePrettyCode from "rehype-pretty-code"
 import rehypeSlug from "rehype-slug"
-import { Transformer } from "unified"
-
-// Define a type alias for the array of plugins
-type RehypePlugin = Transformer<any, any>
 
 export default async function MDXServer(code: string) {
   const result = await bundleMDX({
     source: code,
     mdxOptions(options) {
-      // Define the rehypePlugins array with the correct types
       options.rehypePlugins = [
         ...(options.rehypePlugins ?? []),
-        rehypeSlug as RehypePlugin,
+        rehypeSlug,
         [
           rehypePrettyCode,
           {
             theme: "dracula",
             // The rest of the rehypePrettyCode config
           },
-        ] as [RehypePlugin, { theme: string }],
+        ],
         [
           rehypeAutolinkHeadings,
           {
@@ -29,7 +24,7 @@ export default async function MDXServer(code: string) {
               className: ["hash-anchor"],
             },
           },
-        ] as [RehypePlugin, any],
+        ],
       ]
       return options
     },
