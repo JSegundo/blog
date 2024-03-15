a2d30699f439623fb8fb60e69e5cbe2f83653d48
segundo <segujuan27@hotmail.com>
2024-03-15 13:57:31 +0100

    fix medatada.json and mdx-server build error

--- src/lib/mdx-server.ts
+++ src/lib/mdx-server.ts
@@ -1,22 +1,27 @@
-import { bundleMDX } from "mdx-bundler";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypePrettyCode from "rehype-pretty-code";
-import rehypeSlug from "rehype-slug";
+import { bundleMDX } from "mdx-bundler"
+import rehypeAutolinkHeadings from "rehype-autolink-headings"
+import rehypePrettyCode from "rehype-pretty-code"
+import rehypeSlug from "rehype-slug"
+import { Transformer } from "unified"
+
+// Define a type alias for the array of plugins
+type RehypePlugin = Transformer<any, any>
 
 export default async function MDXServer(code: string) {
   const result = await bundleMDX({
     source: code,
     mdxOptions(options) {
+      // Define the rehypePlugins array with the correct types
       options.rehypePlugins = [
         ...(options.rehypePlugins ?? []),
-        rehypeSlug,
+        rehypeSlug as RehypePlugin,
         [
           rehypePrettyCode,
           {
             theme: "dracula",
             // The rest of the rehypePrettyCode config
           },
-        ],
+        ] as [RehypePlugin, { theme: string }],
         [
           rehypeAutolinkHeadings,
           {
@@ -24,11 +29,11 @@ export default async function MDXServer(code: string) {
               className: ["hash-anchor"],
             },
           },
-        ],
-      ];
-      return options;
+        ] as [RehypePlugin, any],
+      ]
+      return options
     },
-  });
+  })
 
-  return result.code;
+  return result.code
 }
